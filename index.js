25781605e40f95b46614a7c0f3c8b1d0db29de02
Jake16642 <jaketran@gmail.com>
2024-01-17 17:06:51 -0800

    Minor change

--- index.js
+++ index.js
@@ -141,16 +141,15 @@ app.post("/stockprice", async (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render("about.ejs");
+  //res.render("about.ejs");
+  res.render("graph.ejs");
 });
 
 app.get("/contact", (req, res) => {
   res.render("contact.ejs");
 });
 
-app.get("/graph", (req, res) => {
-  res.render("graph.ejs");
-});
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
