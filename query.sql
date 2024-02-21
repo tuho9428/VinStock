6b9811c0f798dc37e346f1fb5e91ee952cc71026
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-02-21 14:25:41 -0800

    update sql

--- query.sql
+++ query.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS stock_data;
+DROP TABLE IF EXISTS favorite_stocks;
+
 CREATE TABLE users(
 id SERIAL PRIMARY KEY,
 email VARCHAR(100) NOT NULL UNIQUE,
@@ -15,4 +19,12 @@ CREATE TABLE stock_data (
     previous_close NUMERIC,
     change_amount NUMERIC,
     change_percent NUMERIC
+);
+
+CREATE TABLE favorite_stocks (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    symbol VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT unique_user_symbol UNIQUE (user_id, symbol)
 );
