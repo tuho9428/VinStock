14657d97d03adafe1cbd7b38d6d8ff8f4ae7aa75
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-02-27 00:29:08 -0800

    edit js files

--- public/scripts/scripto.js
+++ public/scripts/scripto.js
@@ -1,4 +1,3 @@
-alert("hellooooooo");
 function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
