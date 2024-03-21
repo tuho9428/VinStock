e96386e4ddfa0204c357997165bb77ae33d8e2a2
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-03-21 14:54:43 -0700

    update img

--- index.js
+++ index.js
@@ -42,12 +42,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  connectionString: process.env.POSTGRES_URL ,
+  //connectionString: process.env.POSTGRES_URL ,
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  // port: process.env.PG_PORT,
+  port: process.env.PG_PORT,
 });
 
 db.connect();
